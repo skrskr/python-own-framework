71d548014611f1e4112075130fa4730714797615
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-07-06 18:46:22 +0200

    added simple route and handle routes request resposne

--- api.py
+++ api.py
@@ -2,13 +2,38 @@ from webob import Request, Response
 
 
 class API:
+
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
+    
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not Found"
+        return response
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            if path == request_path:
+                return handler
+        return None
+
     def handle_request(self, request):
-        user_agent = request.environ.get('HTTP_USER_AGENT', 'No User Agent Found')
         response = Response()
-        response.text = f"Hello, World! \n\n User Agent: {user_agent}"
+        handler = self.find_handler(request.path)
+        if handler is not None:
+            handler(request, response)
+        else:
+            self.default_response(response)
         return response
--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from api import API
 
 app = API()
+
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello From HOME page"
+
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello From About page"
