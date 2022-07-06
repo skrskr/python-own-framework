1ec498d7df1f4b6706fa07320f66a8ebf2748ffe
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-07-06 17:22:39 +0200

    added reverseware middleware

--- wsgi_demo.py
+++ wsgi_demo.py
@@ -1,6 +1,15 @@
 from wsgiref.simple_server import make_server
 
 
+class Reverseware:
+    def __init__(self, app):
+        self.wrapped_app = app
+
+    def __call__(self, environ, start_response, *args, **kwargs):
+        wrapped_response = self.wrapped_app(environ, start_response, *args, **kwargs)
+        return [data[::-1] for data in wrapped_response]
+
+
 def application(environ, start_response):
     response_body = [
         f'{key}: {value}' for key, value in sorted(environ.items())
@@ -13,5 +22,5 @@ def application(environ, start_response):
     start_response(status, response_headers)
     return [response_body.encode('utf-8')]
 
-server = make_server('localhost', 8000, app=application)
+server = make_server('localhost', 8000, app=Reverseware(application))
 server.serve_forever()
