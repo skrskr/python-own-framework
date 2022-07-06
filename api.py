3e156a7e4bce414007eab43f701142b0532fee9c
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-07-06 17:50:03 +0200

    install webob to use request response classes

--- api.py
+++ api.py
@@ -1,6 +1,14 @@
+from webob import Request, Response
+
+
 class API:
     def __call__(self, environ, start_response):
-        response_body = b"Hello, World!"
-        status = "200 OK"
-        start_response(status, headers=[])
-        return iter([response_body])
+        request = Request(environ)
+        response = self.handle_request(request)
+        return response(environ, start_response)
+
+    def handle_request(self, request):
+        user_agent = request.environ.get('HTTP_USER_AGENT', 'No User Agent Found')
+        response = Response()
+        response.text = f"Hello, World! \n\n User Agent: {user_agent}"
+        return response
