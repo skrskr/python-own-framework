657937e6d82ea5db2233b807c675bfdaabc4b358
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2022-07-18 12:14:51 +0200

    added orm insert statement

--- tests/test_orm.py
+++ tests/test_orm.py
@@ -25,3 +25,36 @@ def test_create_tables(db, Author, Book):
 
     for table in ("author", "book"):
         assert table in db.tables
+
+
+def test_create_author_instance(db, Author):
+    db.create(Author)
+    author = Author(name="John Doe", age=42)
+    
+    assert author.name == "John Doe"
+    assert author.age == 42
+    assert author.id is None
+
+
+def test_save_author_instance(db, Author):
+    db.create(Author)
+    author = Author(name="John Doe", age=42)
+    db.save(author)
+
+    assert author._get_insert_sql() == (
+        "INSERT INTO author (age, name) VALUES (?, ?);",
+        [42, "John Doe"]
+    )
+
+    assert author.id is 1
+
+    author2 = Author(name="John Doe2", age=35)
+    db.save(author2)
+
+    assert author2.id is 2
+
+    author3 = Author(name="John Doe3", age=44)
+    db.save(author3)
+
+    assert author3.id is 3
+    
